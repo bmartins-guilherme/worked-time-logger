2da695c9f72a5fbb5e18612245f261f372734635
Guilherme Bueno Martins <bmartins.guilherme.2022@gmail.com>
2026-07-09 21:00:31 -0300

    fix: Health endpoint

--- user-service/src/main/java/io/github/bmartins_guilherme/user_service/controller/HealthController.java
+++ user-service/src/main/java/io/github/bmartins_guilherme/user_service/controller/HealthController.java
@@ -1,6 +1,5 @@
 package io.github.bmartins_guilherme.user_service.controller;
 
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +11,10 @@ import io.github.bmartins_guilherme.user_service.swagger.HealthSwagger;
 @Controller
 @RequestMapping("/api/v1/health")
 public class HealthController implements HealthSwagger {
+
     @Override
     @GetMapping
     public ResponseEntity<HealthResponse> getHealth() {
-        return new ResponseEntity<HealthResponse>(new HealthResponse(), HttpStatusCode.valueOf(200));
+        return ResponseEntity.ok(new HealthResponse());
     }
 }
--- user-service/src/test/java/io/github/bmartins_guilherme/user_service/controller/HealthControllerTest.java
+++ user-service/src/test/java/io/github/bmartins_guilherme/user_service/controller/HealthControllerTest.java
@@ -2,7 +2,7 @@ package io.github.bmartins_guilherme.user_service.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.json.JsonCompareMode;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,23 +12,26 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
 import io.github.bmartins_guilherme.user_service.dto.HealthResponse;
-import tools.jackson.databind.json.JsonMapper;
 
 @WebMvcTest(HealthController.class)
 public class HealthControllerTest {
-    private final String BASE_URI = "/api/v1/health";
-    private JsonMapper mapper = new JsonMapper();
+    private final String URI = "/api/v1/health";
+    private JsonMapper jsonMapper = new JsonMapper();
     @Autowired
-    private MockMvc mockMvc;
-    
+    private MockMvc mvc;
+
     @Test
     void testGetHealth() throws Exception {
-        ResultMatcher statusCode = MockMvcResultMatchers.status().isOk();
-        final String json = mapper.writeValueAsString(new HealthResponse());
-        ResultMatcher content = MockMvcResultMatchers.content().json(json, JsonCompareMode.STRICT);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get(BASE_URI);
-        requestBuilder.contentType(MediaType.APPLICATION_JSON);
-        mockMvc.perform(requestBuilder).andExpectAll(statusCode, content).andDo(MockMvcResultHandlers.print());
+        // Assertions
+        final ResultMatcher statusCode = MockMvcResultMatchers.status().is(200);
+        final String json = jsonMapper.writeValueAsString(new HealthResponse());
+        final ResultMatcher content = MockMvcResultMatchers.content().json(json, JsonCompareMode.STRICT);
+        // Request
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get(URI);
+        requestBuilder.accept(MediaType.APPLICATION_JSON);
+        mvc.perform(requestBuilder).andExpectAll(statusCode, content).andDo(MockMvcResultHandlers.print());
     }
 }
